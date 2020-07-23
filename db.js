9a476c6069067de0062d2ad697e0e24565b1ffaa
Sarthak Rakhra <sr691312@dal.ca>
2020-07-23 13:43:31 -0300

    Added environment variable for database url call

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const { Client } = require("pg");
 
 const client = new Client({
   connectionString:
-    "postgres://urejtrhpecxngo:d6535cb9ceca727cf6cf8ffeca82f37f2faf8ad84e29368719b146b037c721c8@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d1eens1hiuc999",
+    "postgres://urejtrhpecxngo:d6535cb9ceca727cf6cf8ffeca82f37f2faf8ad84e29368719b146b037c721c8@ec2-3-231-16-122.compute-1.amazonaws.com:5432/d1eens1hiuc999" ||
+    process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
