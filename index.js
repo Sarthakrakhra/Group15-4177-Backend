38ddf12a8c52f5cc83432952ac7c15e91b47710c
Sarthak Rakhra <sr691312@dal.ca>
2020-07-23 14:00:07 -0300

    debug

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const userRoute = require("./api/routes/userRoute");
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use("/user", userRoute);
+
 app.use((req, res, next) => {
   res.send({ Message: "it works!" });
 });
 
-app.use("/user", userRoute);
-
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
 console.log(`Listening on port ${port}`);
