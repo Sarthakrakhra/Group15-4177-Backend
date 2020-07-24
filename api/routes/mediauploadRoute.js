87c3375eae58a407fea2eb129d827d50efe77958
Sally Keating <sl303780@dal.ca>
2020-07-24 10:41:03 -0400

    working on mediaupload route

---
+++ api/routes/mediauploadRoute.js
@@ -0,0 +1,26 @@
+//Author: Sally Keating | B00739692
+var express = require('express');
+var app = express.Router();
+var fileUpload = require('express-fileupload');
+
+app.use(fileUpload());
+
+app.post('/upload', (req, res) => {
+    console.log(req.files.fileToUpload);
+
+    if(req.files === null){
+        console.log("No file uploaded");
+    }
+
+    var file = req.files.file;
+
+    // const queryString = "";
+    // connection.query(queryString, function(error, rows, fields){
+    //     if (error){
+    //         console.log(error);
+    //     }
+    //     res.json(rows);
+    // });
+});
+
+module.exports = app;
