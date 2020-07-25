c469415538eb18509dba0b957da7d9fc1d8c643b
molarknave <skylerb3@hotmail.com>
2020-07-25 20:02:52 -0300

    Created backend for search bar. It searches through thread titles and text and see if it contains the search term

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require("http");
-const port = process.env.port || 3000;
+const port = process.env.port || 3001;
 const bodyParser = require("body-parser");
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const userRoute = require("./api/routes/userRoute");
+const searchRoute = require("./api/routes/searchRoute");
 
 app.use("/user", userRoute);
+app.use("/search", searchRoute);
 
 const server = http.createServer(app);
 server.listen(port);
