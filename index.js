ab73a2710e975f6181c11f02213eb2cd7e7d9f84
molarknave <skylerb3@hotmail.com>
2020-07-25 21:07:16 -0300

    Resolving conflicts in index

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
+/**
+ * @author Sarthak Rakhra, Lauchlan Toal, Sally Keating, Skyler Boutilier
+ */
+
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const http = require("http");
-const port = process.env.port || 3001;
+
 const bodyParser = require("body-parser");
+
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const userRoute = require("./api/routes/userRoute");
+const threadRoute = require("./api/routes/threadRoute");
+const mediaUploadRoute = require("./api/routes/mediauploadRoute");
 const searchRoute = require("./api/routes/searchRoute");
 
 app.use("/user", userRoute);
+app.use("/thread", threadRoute);
+app.use("/upload", mediaUploadRoute);
 app.use("/search", searchRoute);
 
+const forumRoute = require("./api/routes/forumRoute");
+app.use("/forum", forumRoute);
+
+app.use((req, res, next) => {
+  res.send({ Message: "it works!" });
+});
+
+const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 server.listen(port);
+console.log(`Listening on port ${port}`);
